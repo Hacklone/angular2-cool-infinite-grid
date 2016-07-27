f3cdf78f042d7433680ce0e6c0bb1418cd5d75e6
Barna Tóth <toakak@gmail.com>
2016-07-27 22:23:11 +0200

    make component public

--- cool-infinite-grid.ts
+++ cool-infinite-grid.ts
@@ -1,4 +1,5 @@
 import { CoolInfiniteGridComponent } from './src/infinite-grid.component';
+export { CoolInfiniteGridComponent } from './src/infinite-grid.component';
 
 export const COOL_INFINITE_GRID_DIRECTIVES: any[] = [
     CoolInfiniteGridComponent
